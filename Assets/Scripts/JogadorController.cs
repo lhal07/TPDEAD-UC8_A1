fb09d28d6e50a10dbaeb51e420396956ce23eca9
Luis Lourenço <lhal07@gmail.com>
2021-08-09 10:13:47 -0300

    Part1: Add Player movimentation
    - Sprite Renderer
    - Mark Flip Y
    - Set Order in Layer
    - Add Rigidbody2D
    - Set Gravity Scale to 0
    - Add Box Collider 2D
    - Add Player Script
    - Add Player Tag
    - Add empty game object for gun

---
+++ Assets/Scripts/JogadorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JogadorController : MonoBehaviour
+{
+
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    public int Life;
+    public float Speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float translationY = 0;
+        float translationX = Speed;
+        if (Input.GetKey("a")) {
+            transform.Translate((-1*translationX),translationY,0);
+        }
+        if (Input.GetKey("d")) {
+            transform.Translate(translationX,translationY,0);
+        }
+    }
+}
