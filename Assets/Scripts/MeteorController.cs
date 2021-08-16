b1219e7a8b7e22fa88eda14cf5569fe237e10f97
Luis Lourenço <lhal07@gmail.com>
2021-08-16 14:03:27 -0300

    Part5: Destroy non-collected objects
    - Meteors that pass the bottom limit of the screen selfdestruct itself in 0.5s

--- Assets/Scripts/MeteorController.cs
+++ Assets/Scripts/MeteorController.cs
@@ -6,10 +6,12 @@ public class MeteorController : MonoBehaviour
 {
     private Rigidbody2D m_rb;
     private Animator m_anim;
+    private Camera m_camera;
     public JogadorController m_player;
     public float m_Damage = 1.0f;
     public int m_points = 5;
-    private bool destroyed = false;
+    private bool m_Destroyed = false;
+    private float m_ScreenBottomLimit;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +20,16 @@ public class MeteorController : MonoBehaviour
         m_anim = gameObject.GetComponent<Animator>();
         m_player = (JogadorController)FindObjectOfType(typeof(JogadorController));
         m_rb.velocity = new Vector2(0.0f, -1.0f);
+        m_camera = (Camera)FindObjectOfType(typeof(Camera));
+        m_ScreenBottomLimit = m_camera.transform.position.y - (m_camera.rect.height * m_camera.orthographicSize);
     }
 
     void FixedUpdate()
     {
-
+        float meteorPosY = this.transform.position.y;
+        if ((meteorPosY <= m_ScreenBottomLimit) && !m_Destroyed) {
+            DestroyMeteor();
+        }
     } 
 
     // Update is called once per frame
@@ -31,12 +38,17 @@ public class MeteorController : MonoBehaviour
 
     }
 
+    void DestroyMeteor()
+    {
+        m_Destroyed = true;
+        Destroy(this.gameObject,0.5f);
+    }
+
     void Hit()
     {
-        destroyed = true;
         m_rb.velocity = new Vector2(0.0f, 0.0f);
         m_player.AddScore(m_points);
         m_anim.SetTrigger("explode");
-        Destroy(this.gameObject,0.5f);
+        DestroyMeteor();
     }
 }
